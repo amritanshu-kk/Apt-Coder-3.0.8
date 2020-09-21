b53a1859db67cc9292d72b909094870af910ef2c
Amritanshu <amritanshu26dec@gmail.com>
2020-09-21 18:38:41 +0530

    fixed conflict

--- Edunica/settings.py
+++ Edunica/settings.py
@@ -78,8 +78,13 @@ WSGI_APPLICATION = 'Edunica.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'databasename',
+        'USER': 'mysqluser',
+        'PASSWORD': 'mysqlpassword',
+        'HOST': 'localhost',
+        'PORT' : '3306'
+        #'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
--- accounts/views.py
+++ accounts/views.py
@@ -3,14 +3,29 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 from django.shortcuts import render, redirect
-from accounts.forms import RegisterForm
+from .forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 def signin(request):
-	return render(request, 'signin.html')
+	#return render(request, 'signin.html')
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username, password)
+		if user is not None:
+			form = login(request, user)
+			message.succes(request, "welcome")
+			return redirect('index')
+		else:
+			message.info(request, "please login")
+	form = AuthenticationForm()
+	return render(request, 'signin.html', {'form':form} )
 # Create your views here.
-def signup(response):
-    if response.method == "POST":
-	    form = RegisterForm(response.POST)
+
+def signup(request):
+    if request.method == "POST":
+	    form = RegisterForm(request.POST)
 	    if form.is_valid():
 	        form.save()
 
@@ -18,4 +33,8 @@ def signup(response):
     else:
 	    form = RegisterForm()
 
+<<<<<<< HEAD
     return render(response, "signup.html", {"form":form})
+=======
+    return render(request, "signup.html", {"form":form})
+>>>>>>> 546c170... added signup method
